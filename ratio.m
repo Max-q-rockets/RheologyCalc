3a83f6520f4b13dcdccd1baa13f05e5a9f5e4b3d
jetze003 <jetze003@umn.edu>
2021-12-29 19:17:04 -0600

    cleanup

--- ratio.m
+++
@@ -1,8 +0,0 @@
-function rat = ratio(amounts, sizes, n, ex)
-        Ratio = @(FC, FM, c, m) FM-abs((FM-FC)*(c/(m+c))^(1/(ex)));
-        ratios = sizes(n)./sizes;
-        rat = 1;
-        for i=1:n-1
-            rat = Ratio(rat, ratios(i), sum(amounts(1:i-1)), amounts(i));
-        end
-    end
