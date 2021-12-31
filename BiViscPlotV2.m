fd2fa7b3e985061558585a96de315bc6b4d59632
jetze003 <jetze003@umn.edu>
2021-12-30 20:51:10 -0600

    updates

--- BiViscPlotV2.m
+++ BiViscPlotV2.m
@@ -2,17 +2,18 @@ function Output = BiViscPlotV2(amounts, sizes, densities, liqdens, ind, plotyn)
 
 C = [0:.001:1];
 sum = amounts(ind(1))+amounts(ind(2));
-Output = 1./zeros(length(C));
+Output = 1./zeros(length(C), 1);
 for i=1:length(C)
     amounts1 = amounts;
     amounts1(ind(1)) = sum*C(i);
     amounts1(ind(2)) = sum*(1-C(i));
     Output(i) = visc2(amounts1, sizes, densities, liqdens);
 end
+figure();
 semilogy(C, Output);
 hold on
-opt = min(min(Output));
-ylim([1, opt*10^3]);
+opt = min(Output);
+ylim([1, min([opt*10^3, max(Output)])]);
 if plotyn == 'y'
     x = (amounts(ind(1)))/(amounts(ind(1))+amounts(ind(2)));
     y= visc2(amounts, sizes, densities, liqdens);
--- BiViscPlotV3.m
+++ BiViscPlotV3.m
@@ -2,17 +2,18 @@ function Output = BiViscPlotV3(amounts, sizes, densities, SParams, liqdens, ind,
 
 C = [0:.001:1];
 sum = amounts(ind(1))+amounts(ind(2));
-Output = 1./zeros(length(C));
+Output = 1./zeros(length(C), 1);
 for i=1:length(C)
     amounts1 = amounts;
     amounts1(ind(1)) = sum*C(i);
     amounts1(ind(2)) = sum*(1-C(i));
     Output(i) = visc3(amounts1, sizes, densities, SParams, liqdens);
 end
+figure();
 semilogy(C, Output);
 hold on
 opt = min(min(Output));
-ylim([1, opt*10^3]);
+ylim([1, min([opt*10^3, max(Output)])]);
 if plotyn == 'y'
     x = (amounts(ind(1)))/(amounts(ind(1))+amounts(ind(2)));
     y= visc3(amounts, sizes, densities, SParams, liqdens);
